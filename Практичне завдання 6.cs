a09e8b8b7a1c5dd9d53b67256c28200da8eb859b
netrebkom <118273809+netrebkom@users.noreply.github.com>
2023-04-29 16:14:02 +0300

    Add files via upload

---
+++ Практичне завдання 6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        string[] lines = File.ReadAllLines("input.txt");
+     
+        string[] sortedLines = lines.OrderBy(line => line).ToArray();
+      
+        File.WriteAllLines("output.txt", sortedLines);
+    }
+}
